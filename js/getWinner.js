a534e11d9285a96a85e118d01a23657f03a431c9
Your NameJoseph <you@example.comjosephracca@gmail.com>
2021-01-07 14:51:01 -0800

    cleaned for review

--- js/getWinner.js
+++ js/getWinner.js
@@ -12,7 +12,6 @@ let roundWinner = document.getElementById("roundWinner");
 let cpuBtn = document.getElementById("cpuBtn");
 
 function getWinner(array, numRounds, thresh, playerName) {
-  // console.log(`rounds left: ${roundsLeft}`);
   let p1 = array[0];
   let p2 = array[1];
 
@@ -30,32 +29,20 @@ function getWinner(array, numRounds, thresh, playerName) {
       player1pts++;
       roundWinner.innerText = `Player 1 wins the round! ${p1} beats ${p2}`;
     } else {
-      // console.log(player2pts);
       player2pts++;
-      // console.log(player2pts);
-      // console.log(array);
       roundWinner.innerText = `${playerName} wins the round! ${p2} beats ${p1}`;
     }
-
     roundsLeft--;
     // return numRounds;
   }
   p1Points.innerText = player1pts;
   p2Points.innerText = player2pts;
-
-  // console.log(`rounds left after: ${numRounds}`);
-
   checkRoundsLeft(numRounds, player1pts, player2pts, thresh, playerName);
 }
 
 let roundsToPlay = 0;
 
 function checkRoundsLeft(numRoundsIn, p1Points, p2Points, winningPoints, user) {
-  // stop game when it hits 0 rounds left
-  // console.log(
-  //   `${numRoundsIn} : ${p1Points} : ${p2Points} : ${winningPoints} : ${user}`
-  // );
-
   if (
     p1Points === winningPoints ||
     p2Points === winningPoints ||
@@ -86,19 +73,15 @@ function gamePlay(real, rounds, points, playerName) {
   player1pts = 0;
   player2pts = 0;
 
-
   async function getAIChoice(url) {
     let choice = await fetch(url);
     let choice2 = await choice.text();
     cpuChoice = choice2;
-    // console.log(cpuChoice);
   }
 
   getAIChoice(AIChoiceURL);
 
   roundsLeft = rounds;
-
-  // console.log(`to win: ${points} rounds: ${roundsLeft}`);
 
   let confirmBtn = document.getElementById("confirmBtn");
   let contBtn = document.getElementById("contBtn");
@@ -110,33 +93,44 @@ function gamePlay(real, rounds, points, playerName) {
     choicePane.classList.add("d-none");
   }
 
-  let selectCharBtn = document.getElementById("selectCharBtn").addEventListener("click", function () {
+  let selectCharBtn = document
+    .getElementById("selectCharBtn")
+    .addEventListener("click", function () {
       readyPane.classList.add("d-none");
       choicePane.classList.remove("d-none");
     });
 
   //CHARACTER SELECTION
-  let rockBtn = document.getElementById("rockBtn").addEventListener("click", function () {
+  let rockBtn = document
+    .getElementById("rockBtn")
+    .addEventListener("click", function () {
       character = "rock";
     });
-  let paperBtn = document.getElementById("paperBtn").addEventListener("click", function () {
+  let paperBtn = document
+    .getElementById("paperBtn")
+    .addEventListener("click", function () {
       character = "paper";
     });
-  let scissorsBtn = document.getElementById("scissorsBtn").addEventListener("click", function () {
+  let scissorsBtn = document
+    .getElementById("scissorsBtn")
+    .addEventListener("click", function () {
       character = "scissors";
     });
-  let lizardBtn = document.getElementById("lizardBtn").addEventListener("click", function () {
+  let lizardBtn = document
+    .getElementById("lizardBtn")
+    .addEventListener("click", function () {
       character = "lizard";
     });
-  let spockBtn = document.getElementById("spockBtn").addEventListener("click", function () {
+  let spockBtn = document
+    .getElementById("spockBtn")
+    .addEventListener("click", function () {
       character = "spock";
     });
 
   confirmBtn.addEventListener("click", function () {
-    // console.log(player2pts);
     if (real) {
       if (!character) {
-        alert('select a character first!');
+        alert("select a character first!");
       } else if (charactersSelected.length === 0) {
         pushName();
 
@@ -145,18 +139,15 @@ function gamePlay(real, rounds, points, playerName) {
       } else {
         pushName();
         //then run comparison
-        // console.log(`${roundsLeft}`);
         getWinner(charactersSelected, roundsLeft, points, playerName);
-        // console.log(`${roundsLeft}`);
       }
 
       character = "";
     } else {
       if (!character) {
-        alert('select a character first!');
+        alert("select a character first!");
       } else if (charactersSelected.length === 0) {
         pushName();
-        // console.log(charactersSelected);
 
         cpuBtn.classList.remove("d-none");
         confirmBtn.classList.add("d-none");
@@ -164,20 +155,10 @@ function gamePlay(real, rounds, points, playerName) {
         character = cpuChoice.toLowerCase();
         // character = 'rock'; to test single variable
         pushName();
-          // console.log(charactersSelected);
-
-          confirmBtn.classList.remove("d-none");
-
-          // character = cpuChoice;
-          // pushName();
-          // console.log(charactersSelected);
-
-
-          // console.log(`${roundsLeft}`);
-          getWinner(charactersSelected, roundsLeft, points, playerName);
-          // console.log(`${roundsLeft}`);
-          cpuBtn.classList.add("d-none");
-          charactersSelected = [];
+        confirmBtn.classList.remove("d-none");
+        getWinner(charactersSelected, roundsLeft, points, playerName);
+        cpuBtn.classList.add("d-none");
+        charactersSelected = [];
 
         // cpuBtn.addEventListener("click", function () {}); SHOULD YOU WANT TO USE A SEPARATE BUTTON <------
       }
@@ -191,8 +172,6 @@ function gamePlay(real, rounds, points, playerName) {
     pNum.innerText = "1";
     charactersSelected = [];
   });
-
-  // console.log(`to win: ${points}`);
 }
 
 export { getWinner, gamePlay };
